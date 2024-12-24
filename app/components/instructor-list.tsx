59376658ad3d23888cffafe881bd33a14f855c2d
ANTON HANZHIN <cmd720@gmail.com>
2024-12-24 14:18:17 +0500

    err
    remove err

--- app/components/instructor-list.tsx
+++ app/components/instructor-list.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, {useState} from 'react';
-import Instructors from "@/app/components/instructors";
 import Person from "@/app/components/person";
 
 type TInstructor = {
